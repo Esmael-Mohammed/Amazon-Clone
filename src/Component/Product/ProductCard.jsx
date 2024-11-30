7093383bcdeb15707fd9141e30f9908ae3178645
Esmael-Mohammed <esmaelmohammed01@gmail.com>
2024-11-30 14:22:59 +0300

    base url

--- src/Component/Product/ProductCard.jsx
+++ src/Component/Product/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { Rating } from '@mui/material'
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
-import classes from './product.module.css'
+import classes from './Product.module.css'
 import { Link } from 'react-router-dom'
 import { DataContext } from '../DataProvider/DataProvider'
 import { Type } from '../../utils/action.type'
